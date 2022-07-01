c7319e8ee48c17a5d3687e8124ec403c8d9f737d
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-01 21:55:42 +0530

    fetch api function added

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+
+  return data;
+};
