f26cf42423fdda2e205c37bf7c41110d98fcd05c
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-01 22:05:51 +0530

    App style created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
 
 const App = () => {
+  const [query, setQuery] = useState("");
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      console.log(data);
+    }
+  };
+
   return (
-    <div>
-      <h1>App</h1>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search your Location:"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
     </div>
   );
 };
