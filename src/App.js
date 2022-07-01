217cdca762e393324341999a8af7cb7a47c16e20
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-01 22:13:37 +0530

    output card created

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import "./App.css";
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
   const search = async (e) => {
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
 
-      console.log(data);
+      setWeather(data);
+      setQuery("");
     }
   };
 
@@ -24,6 +26,27 @@ const App = () => {
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
+
+      {weather?.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
